34b29abf04180b54a250d091b0b177141b253a93
ashbo2 <62220098+ashbo2@users.noreply.github.com>
2020-11-30 10:24:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,44 +10,68 @@ var config = {
     footer: 'Source: City of Chicago Data Portal',
     chapters: [
       {
-          id: 'slug-style-id-1',
+          id: 'id-chicago-blank',
           title: 'Chicago',
           // image: './path/to/image/source.png',
           description: '',
           location: {
-              center: [-87.62584, 41.87269],
-              zoom: 9,
+              center: [-87.560, 41.823],
+              zoom: 8,
               pitch: 0,
               bearing: 0
           },
           onChapterEnter: [
                {
-                   layer: 'affordable-rental-housing-dev-31ve8k',
+                   layer: 'cleaned-city-owned-inventory-080a1x',
                    opacity: 0
                }
           ],
           onChapterExit: [
                {
-                   layer: 'affordable-rental-housing-dev-31ve8k',
+                   layer: 'cleaned-city-owned-inventory-080a1x',
                    opacity: 0
                }
           ]
       },
       {
-            id: 'id-rentburden',
+          id: 'id-city-owned-land',
+          title: 'City-Owned Land Inventory',
+          // image: './path/to/image/source.png',
+          description: 'Current city-owned land inventory in Chicago. This land could serve as potential sites for new affordable housing.',
+          location: {
+              center: [-87.560, 41.823],
+              zoom: 10,
+              pitch: 0,
+              bearing: 0
+          },
+          onChapterEnter: [
+               {
+                   layer: 'cleaned-city-owned-inventory-080a1x',
+                   opacity: 1
+               }
+          ],
+          onChapterExit: [
+               {
+                   layer: 'cleaned-city-owned-inventory-080a1x',
+                   opacity: 1
+               }
+          ]
+      },
+      {
+            id: 'id-rentburden-community',
             title: 'Rent Burden',
             // image: './path/to/image/source.png',
             description: 'Chloropeth Map of rent burden by community area.',
             location: {
-                center: [-87.62584, 41.87269],
-                zoom: 11,
+                center: [-87.560, 41.823],
+                zoom: 10,
                 pitch: 0,
                 bearing: 0
             },
             onChapterEnter: [
                  {
                      layer: 'sea10026-by-community-area-d89drz',
-                     opacity: 1
+                     opacity: 0.75
                  }
             ],
             onChapterExit: [
@@ -58,20 +82,68 @@ var config = {
             ]
         },
         {
-            id: 'id-evictions',
+              id: 'id-rentburden-community-zoomed',
+              title: 'Rent Burden',
+              // image: './path/to/image/source.png',
+              description: 'Chloropeth Map of rent burden by community area.',
+              location: {
+                  center: [-87.560, 41.823],
+                  zoom: 11.5,
+                  pitch: 0,
+                  bearing: 0
+              },
+              onChapterEnter: [
+                   {
+                       layer: 'sea10026-by-community-area-d89drz',
+                       opacity: 0.75
+                   }
+              ],
+              onChapterExit: [
+                   {
+                       layer: 'sea10026-by-community-area-d89drz',
+                       opacity: 0
+                   }
+              ]
+          },
+          {
+                id: 'id-rentburden-tract',
+                title: 'Rent Burden',
+                // image: './path/to/image/source.png',
+                description: 'Chloropeth Map of rent burden by Census tract.',
+                location: {
+                    center: [-87.560, 41.823],
+                    zoom: 11.5,
+                    pitch: 0,
+                    bearing: 0
+                },
+                onChapterEnter: [
+                     {
+                         layer: 'sea10026-w-tracts-by-communit-71pqil',
+                         opacity: 0.75
+                     }
+                ],
+                onChapterExit: [
+                     {
+                         layer: 'sea10026-w-tracts-by-communit-71pqil',
+                         opacity: 0
+                     }
+                ]
+            },
+        {
+            id: 'id-evictions-community',
             title: 'Evictions',
             // image: './path/to/image/source.png',
             description: 'Chloropeth Map of evictions by community area.',
             location: {
-                center: [-87.62584, 41.87269],
-                zoom: 11,
+                center: [-87.560, 41.823],
+                zoom: 10,
                 pitch: 0,
                 bearing: 0
             },
             onChapterEnter: [
                  {
                      layer: 'evictions-commarea',
-                     opacity: 1
+                     opacity: 0.75
                  }
             ],
             onChapterExit: [
@@ -82,12 +154,60 @@ var config = {
             ]
         },
         {
-            id: 'slug-style-id',
+            id: 'id-evictions-community-zoomed',
+            title: 'Evictions',
+            // image: './path/to/image/source.png',
+            description: 'Chloropeth Map of evictions by community area.',
+            location: {
+                center: [-87.560, 41.823],
+                zoom: 11.5,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                 {
+                     layer: 'evictions-commarea',
+                     opacity: 0.75
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'evictions-commarea',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'id-evictions-tract',
+            title: 'Evictions',
+            // image: './path/to/image/source.png',
+            description: 'Chloropeth Map of evictions by Census tract.',
+            location: {
+                center: [-87.560, 41.823],
+                zoom: 11.5,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                 {
+                     layer: 'eviction-tract',
+                     opacity: 0.75
+                 }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'eviction-tract',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'id-affordable-housing',
             title: 'Affordable Rental Housing Developments',
             // image: './path/to/image/source.png',
             description: 'Current affordable rental housing developments in the city of Chicago. This shows the efforts already in place by the city to address the affordability crisis.',
             location: {
-                center: [-87.62584, 41.87269],
+                center: [-87.560, 41.823],
                 zoom: 11,
                 pitch: 0,
                 bearing: 0
@@ -101,30 +221,6 @@ var config = {
             onChapterExit: [
                  {
                      layer: 'affordable-rental-housing-dev-31ve8k',
-                     opacity: 0
-                 }
-            ]
-        },
-        {
-            id: 'other-identifier',
-            title: 'City-Owned Land Inventory',
-            // image: './path/to/image/source.png',
-            description: 'Current city-owned land inventory in Chicago. This land could serve as potential sites for new affordable housing.',
-            location: {
-                center: [-87.62584, 41.87269],
-                zoom: 11,
-                pitch: 0,
-                bearing: 0
-            },
-            onChapterEnter: [
-                 {
-                     layer: 'cleaned-city-owned-inventory-080a1x',
-                     opacity: 1
-                 }
-            ],
-            onChapterExit: [
-                 {
-                     layer: 'cleaned-city-owned-inventory-080a1x',
                      opacity: 0
                  }
             ]
@@ -135,7 +231,7 @@ var config = {
             // image: './path/to/image/source.png',
             description: 'Grocery Stores since 2013.',
             location: {
-                center: [-87.62584, 41.87269],
+                center: [-87.560, 41.823],
                 zoom: 11,
                 pitch: 0,
                 bearing: 0
@@ -159,8 +255,8 @@ var config = {
             // image: './path/to/image/source.png',
             description: 'All CTA Bus Stops',
             location: {
-                center: [-87.62584, 41.87269],
-                zoom: 10,
+                center: [-87.560, 41.823],
+                zoom: 11,
                 pitch: 0,
                 bearing: 0
             },
